d713fc1bf37b5b53cb0b1b60987e3aa2f3d64d3e
Michael <michael.elliston95@gmail.com>
2025-11-06 09:39:28 -0600

    Finished part 1 of the Streams exercise - added sorting method to compare by age, and displays the required information for MVP - created a for loop for filtering and creating a new list based on user input, and gets the average age of everyone in the original list.

--- src/main/java/com/pluralsight/streams/Person.java
+++ src/main/java/com/pluralsight/streams/Person.java
@@ -1,6 +1,6 @@
 package com.pluralsight.streams;
 
-public class Person {
+public class Person implements Comparable<Person> {
     private String firstName;
     private String lastName;
     private int age;
@@ -9,5 +9,27 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName + ", age " + this.age;
+    }
+
+    @Override
+    public int compareTo(Person otherPerson) {
+        return Integer.compare(this.age, otherPerson.age);
     }
 }
--- src/main/java/com/pluralsight/streams/Program.java
+++ src/main/java/com/pluralsight/streams/Program.java
@@ -1,10 +1,54 @@
 package com.pluralsight.streams;
 
+import utilities.InputGetter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
 
 
+        List<Person> people = new ArrayList<Person>(List.of(
+                new Person("Avery", "Bennett", 17),
+                new Person("Mateo", "Cortez", 22),
+                new Person("Maya", "Singh", 18),
+                new Person("Elias", "Romero", 19),
+                new Person("Harper", "Nguyen", 24),
+                new Person("Noah", "Patel", 28),
+                new Person("Isla", "Chen", 27),
+                new Person("Mateo", "Brooks", 25),
+                new Person("Zara", "Thompson", 19),
+                new Person("Caleb", "Foster", 21)
+        ));
+
+        String userInput = InputGetter.getString("Please enter the name you are searching for: ");
+
+        List<Person> filteredPeople = new ArrayList<Person>();
+
+        for (Person person : people) {
+            if (person.getFirstName().equalsIgnoreCase(userInput) || person.getLastName().equalsIgnoreCase(userInput)) {
+                filteredPeople.add(person);
+            }
+        }
+
+        for (Person person : filteredPeople) {
+            System.out.println(person);
+        }
+
+        int ageSum = 0;
+        for (Person person : people) {
+            ageSum += person.getAge();
+        }
+
+        System.out.println("\nAverage age of everyone: " + ageSum / people.size());
+
+        Collections.sort(people);
+
+        System.out.println("Youngest person: " + people.get(0));
+        System.out.println("Oldest person: " + people.get(people.size() - 1));
 
     }
 }
